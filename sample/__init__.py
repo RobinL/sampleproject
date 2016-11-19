0f282127af271c6a000d6337e6c2b75788458449
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2016-11-19 17:22:22 +0000

    works

--- sample/__init__.py
+++ sample/__init__.py
@@ -1,7 +1,8 @@
-
+import pandas as pd 
+import numpy as np
 def main():
     """Entry point for the application script"""
     print("Call your main application code here")
 
 def get_random_df():
-	return df
+	return pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
